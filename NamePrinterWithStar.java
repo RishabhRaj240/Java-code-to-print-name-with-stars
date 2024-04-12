5aed86c7ddf6da3c466e93df77a3f5baac735334
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-12 15:35:32 +0530

    Add files via upload

---
+++ NamePrinterWithStar.java
@@ -0,0 +1,7 @@
+class NamePrinterWithStar {
+    public static void main(String[] args) {
+        System.out.println(" ************* ");
+        System.out.println("* Rishabh Raj *");
+        System.out.println(" ************* ");
+    }
+}
